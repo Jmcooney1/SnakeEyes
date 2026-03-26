bd79c9c0bad263c59e3ba88cffd22825f4d0df95
CGrimm <cgrimm@trinity.edu>
2026-03-26 17:15:08 -0500

    changed to 3-page layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,33 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Tabs } from "expo-router";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+ return (
+   <Tabs>
+     <Tabs.Screen
+       name="index"
+       options={{
+         title: "",
+         tabBarIcon: ({ color }) => <MaterialIcons size={28} name="home-filled" color={color} />,
+         headerShown: false
+       }}
+     />
+     <Tabs.Screen
+       name="create"
+       options={{
+         title: "",
+         tabBarIcon: ({ color }) => <MaterialIcons size={28} name="add" color={color} />,
+         headerShown: false
+       }}
+     />
+     <Tabs.Screen
+       name="settings"
+       options={{
+         title: "",
+         tabBarIcon: ({ color }) => <MaterialIcons size={28} name="settings" color={color} />,
+         headerShown: false
+       }}
+     />
+   </Tabs>
+ )
 }
--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,8 +1,9 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
+
 import { Link } from 'expo-router';
 
-export default function HomeScreen() {
+export default function CreateScreen() {
   return (
     <>
     </>
