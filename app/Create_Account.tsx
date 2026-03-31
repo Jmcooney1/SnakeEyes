be3d6471f0faa329759b5b020a90ca48aebac84a
Jace Cooney <marymargaretcooney39@gmail.com>
2026-03-31 14:22:25 -0500

    Renamed the sign_up file to Create_account

--- app/Create_Account.tsx
+++ app/Create_Account.tsx
@@ -30,7 +30,7 @@ export default function SignUp() {
                 />
         </View>
 
-            <Button title="Have an account? Log In" onPress={() => router.push('/sign_in')}/>
+            <Button title="Have an account? Log In" onPress={() => router.push('/Create_Account')}/>
             <Button title="Create Account" onPress={() => {router.push('/')}}/>
                 
         </View>
